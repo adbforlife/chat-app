13509cb5931f0613aeac362edfcccc9245aa40b5
Albert Gao <adbforlife2018@gmail.com>
2019-01-03 11:46:07 +0800

    Add username changing functionality

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,34 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import io from 'socket.io-client';
-import MessageBox from './MessageBox'
+import MessageBox from './MessageBox';
+import NameEnterField from './NameEnterField';
 const socket = io('http://localhost:5001');
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      username: '',
       test: "gg",
       response: false,
       endpoint: "http:127.0.0.1:5001"
     }
+    this.setState = this.setState.bind(this);
+  }
+
+  changeUserName = (name) => {
+    this.setState({
+      username: name
+    });
   }
 
   componentDidMount() {
+    socket.emit('join', JSON.stringify({
+      'username': this.state.username,
+      'room': 'room'
+    }));
+
     //socket.emit('message', "fuck");
     socket.on('message', (msg) => {
       this.setState({
@@ -25,9 +39,15 @@ class App extends Component {
   }
 
   render() {
+    let greeting;
+    if (this.state.username) {
+      greeting = <h1>Hello, {this.state.username}!</h1>;
+    }
     return (
       <div className="App">
         <header className="App-header">
+          {greeting}
+          <NameEnterField onUpdate={this.changeUserName} />
           <MessageBox socket={socket} />
           <p></p><p></p><p></p>
           <img src={logo} className="App-logo" alt="logo" />
--- server/hello.py
+++ server/hello.py
@@ -1,18 +1,68 @@
 from flask import Flask, url_for
-from flask_socketio import SocketIO, send
+from flask_socketio import SocketIO, send, join_room, leave_room
+import json
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
 socketio = SocketIO(app)
 
-#@socketio.on('join')
-#def on_join(data):
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # Here we use a class of some kind to represent and validate our
+    # client-side form data. For example, WTForms is a library that will
+    # handle this for us, and we use a custom LoginForm to validate.
+    form = LoginForm()
+    if form.validate_on_submit():
+        # Login and validate the user.
+        # user should be an instance of your `User` class
+        login_user(user)
+
+        flask.flash('Logged in successfully.')
+
+        next = flask.request.args.get('next')
+        # is_safe_url should check if the url is safe for redirects.
+        # See http://flask.pocoo.org/snippets/62/ for an example.
+        if not is_safe_url(next):
+            return flask.abort(400)
+
+        return flask.redirect(next or flask.url_for('index'))
+    return flask.render_template('login.html', form=form)
 
 
 @socketio.on('message')
-def handleMessage(msg):
+def handle_message(msg):
 	print('Message: ' + msg)
-	send(msg, broadcast=True)
+	emit('message', msg, broadcast=True)
+
+@socketio.on('connect')
+def on_connect():
+    print('user connected')
+
+'''@socket.on('activate_user')
+def on_active_user(data):
+    user = data.get('username')
+    emit('user_activated', {'user': user}, broadcast=True)'''
+
+
+@socketio.on('join')
+def on_join(data):
+	data = json.loads(data)
+	print(data['username'] + ' joining')
+	username = data['username']
+	if not username:
+		return False
+	room = data['room']
+	join_room(room)
+	send(username + ' has entered the room.' + room + '\n\n', room=room)
+
+@socketio.on('leave')
+def on_leave(data):
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    send(username + ' has left the room.', room=room)
 
 
 if __name__ == '__main__':
